52708a5928e8269c939d77cb83c17cd3f3e0c384
Trevor <thod0127@740s-pl-130.local>
2015-11-02 13:06:35 -0700

    Finished the app for the rubric

--- app/src/main/java/com/example/thod0127/worldsmostbaasictextingapp/TextingActivity.java
+++ app/src/main/java/com/example/thod0127/worldsmostbaasictextingapp/TextingActivity.java
@@ -7,12 +7,21 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class TextingActivity extends AppCompatActivity
 {
+    private TextView sentText;
     private EditText smsMessageField;
     private EditText smsNumberField;
     private Button sendSMSButton;
+    private Button number1;
+    private Button number2;
+    private Button randomMessage;
+    private String trevorNumber;
+    private String emilyNumber;
+    private ArrayList<String> randomText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,7 +32,15 @@ public class TextingActivity extends AppCompatActivity
         sendSMSButton = (Button) findViewById(R.id.sendButton);
         smsMessageField = (EditText) findViewById(R.id.messageBox);
         smsNumberField = (EditText) findViewById(R.id.numberBox);
+        number1 = (Button) findViewById(R.id.number1);
+        number2 = (Button) findViewById(R.id.number2);
+        randomMessage = (Button) findViewById(R.id.niceStuff);
+        sentText = (TextView) findViewById(R.id.sentText);
+        trevorNumber = "8016188606";
+        emilyNumber = "8012050112";
+        randomText = new ArrayList<>();
 
+        setupList();
         setupListeners();
     }
 
@@ -50,11 +67,49 @@ public class TextingActivity extends AppCompatActivity
             }
         });
 
+        number1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View currentView)
+            {
+                smsNumberField.setText(trevorNumber);
+            }
+        });
+
+        number2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View currentView)
+            {
+                smsNumberField.setText(emilyNumber);
+            }
+        });
+
+        randomMessage.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View currentView)
+            {
+                int messageSlot = (int)(Math.random() * randomText.size());
+                String message = randomText.get(messageSlot);
+                smsMessageField.setText(message);
+            }
+        });
     }
 
-    private void sendSMS(String messageAdress, String messageContent)
+    private void sendSMS(String messageAddress, String messageContent)
     {
         SmsManager mySMSManager = SmsManager.getDefault();
-        mySMSManager.sendTextMessage(messageAdress, null, messageContent, null, null);
+        mySMSManager.sendTextMessage(messageAddress, null, messageContent, null, null);
+        sentText.setText(messageContent);
+    }
+
+    private void setupList()
+    {
+        randomText.add("Hello!");
+        randomText.add("Have a good day!");
+        randomText.add("Have a great Summer / Winter / Spring / Fall!");
+        randomText.add("Merry Holidays!");
+        randomText.add("C u l8r!");
     }
 }
